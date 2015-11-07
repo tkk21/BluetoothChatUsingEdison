55c76ff8763a4bfbabb126148fd3f114a8563ebe
Ted Kim <tkk21@case.edu>
2015-11-06 21:32:41 -0500

    read as well

--- Grove_Indoor_Environment_Demo/Grove_Indoor_Environment_Demo.ino
+++ Grove_Indoor_Environment_Demo/Grove_Indoor_Environment_Demo.ino
@@ -532,6 +532,26 @@ void writeToSD(){
   }
   myFile.close();
   Serial.println("Done writing to file");
+
+  readSD();
+}
+
+void readSD(){
+  // re-open the file for reading:
+  myFile = SD.open("test.txt");
+  if (myFile) {
+    Serial.println("test.txt:");
+    
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
 }
 
 int cmdstrInput(char *str){
