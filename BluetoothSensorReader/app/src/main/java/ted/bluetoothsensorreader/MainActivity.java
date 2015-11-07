8973811edd08711b6bcbc25443d5dc75898ae507
Ted Kim <tkk21@case.edu>
2015-11-07 17:10:40 -0500

    added support:appcompat to gradle

--- BluetoothSensorReader/app/src/main/java/ted/bluetoothsensorreader/MainActivity.java
+++ BluetoothSensorReader/app/src/main/java/ted/bluetoothsensorreader/MainActivity.java
@@ -1,18 +1,22 @@
 package ted.bluetoothsensorreader;
 
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (savedInstanceState == null){
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+        }
     }
 
     @Override
