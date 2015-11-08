15a661534203ecd86273bb311402c375282b8f08
Ted Kim <tkk21@case.edu>
2015-11-07 23:39:11 -0500

    wrote CSV writer

---
+++ BluetoothSensorReader/app/src/main/java/ted/bluetoothsensorreader/SensorCSVWriter.java
@@ -0,0 +1,67 @@
+package ted.bluetoothsensorreader;
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Created by ted on 11/7/2015.
+ */
+public class SensorCSVWriter {
+    private static final String TAG = "SensorCSVWriter";
+    private static final String attributeLine = "temp,humi,light,uv,pir,moist";
+    private final String filename;
+    private BufferedWriter bufferedWriter;
+
+    public SensorCSVWriter(){
+        filename = "EdisonSensor.csv";
+    }
+
+    public void writeLine(String data){
+        initialize();
+        try{
+            bufferedWriter.append(data);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+        }
+        catch (IOException e){
+            Log.wtf(TAG, "IOException while writing a line", e);
+        }
+    }
+
+    public void close(){
+        try{
+            bufferedWriter.close();
+        }
+        catch (IOException e){
+            Log.wtf(TAG, "IOException", e);
+        }
+    }
+
+    private void initialize(){
+        if (bufferedWriter != null){
+            return;
+        }
+        String root = Environment.getExternalStorageDirectory().toString();
+        File csvDir = new File(root + "/IntelEdison/");
+        csvDir.mkdir();
+        File file = new File (csvDir, filename);
+        boolean fileExists = file.exists();
+        try {
+            bufferedWriter = new BufferedWriter(new FileWriter(file, true));
+            if (!fileExists) {
+                bufferedWriter.write(attributeLine);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+            }
+        }
+        catch (IOException e){
+            Log.wtf(TAG, "IOExceptio", e);
+        }
+        Log.d(TAG, String.format("File is at: %s", file.getAbsolutePath()));
+    }
+}
