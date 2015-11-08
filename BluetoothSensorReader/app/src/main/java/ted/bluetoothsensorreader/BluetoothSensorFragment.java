0669faeb67a0de55eed97733585ba50d602036d0
Ted Kim <tkk21@case.edu>
2015-11-07 19:47:32 -0500

    added connected thread

--- BluetoothSensorReader/app/src/main/java/ted/bluetoothsensorreader/BluetoothSensorFragment.java
+++ BluetoothSensorReader/app/src/main/java/ted/bluetoothsensorreader/BluetoothSensorFragment.java
@@ -1,23 +1,42 @@
 package ted.bluetoothsensorreader;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
 
 /**
  * Created by ted on 11/7/2015.
  */
 public class BluetoothSensorFragment extends Fragment {
     private static final String TAG = "BluetoothSensorFragment";
-
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+    // Intent request codes
+    private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
+    private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
+    private static final int REQUEST_ENABLE_BT = 3;
 
     /**
      * Local bluetooth adapter
      */
     private BluetoothAdapter mBluetoothAdapter = null;
+    private final Handler mHandler = new Handler(){
+
+    };
+    private final ConnectedThread mConnectedThread;
 
     @Override
     public void onCreate (Bundle savedInstanceState){
@@ -34,5 +53,92 @@ public class BluetoothSensorFragment extends Fragment {
         }
     }
 
-    
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        if (!mBluetoothAdapter.isEnabled()){
+            Intent enableIntent = new Intent (BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        }
+        //else if start the fragment
+    }
+
+    public synchronized void connectToSensorServer(){
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+    }
+
+    public synchronized void stop(){
+
+    }
+
+    private class ConnectedThread extends Thread{
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+        private static final int MESSAGE_READ = 2;
+
+        public ConnectedThread(BluetoothSocket socket, String socketType) {
+            Log.d(TAG, "create ConnectedThread: " + socketType);
+            mmSocket = socket;
+            InputStream tmpInStream = null;
+            OutputStream tmpOutStream = null;
+
+            // Get the BluetoothSocket input and output streams
+            try {
+                tmpInStream = socket.getInputStream();
+                tmpOutStream = socket.getOutputStream();
+            } catch (IOException e) {
+                Log.e(TAG, "temp sockets not created", e);
+            }
+
+            mmInStream = tmpInStream;
+            mmOutStream = tmpOutStream;
+        }
+
+        public void run() {
+            Log.i(TAG, "BEGIN mConnectedThread");
+            byte[] buffer = new byte[1024];
+            int bytes;
+
+            // Keep listening to the InputStream while connected
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    bytes = mmInStream.read(buffer);
+
+                    // Send the obtained bytes to the UI Activity
+                    mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer)
+                            .sendToTarget();
+                } catch (IOException e) {
+                    Log.e(TAG, "disconnected", e);
+                    break;
+                }
+            }
+        }
+
+        /**
+         * Write to the connected OutStream.
+         *
+         * @param buffer The bytes to write
+         */
+        public void write(byte[] buffer) {
+            try {
+                mmOutStream.write(buffer);
+            } catch (IOException e) {
+                Log.e(TAG, "Exception during write", e);
+            }
+        }
+
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "close() of connect socket failed", e);
+            }
+        }
+    }
+
+
+
 }
